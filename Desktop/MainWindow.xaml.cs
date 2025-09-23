282d1f47e5bd2a403f34b6f7532c98d782391403
HypBed <uymaevymat@gmail.com>
2025-09-23 20:35:37 +0300

    Добавил проверку для окна входа

--- Desktop/MainWindow.xaml.cs
+++ Desktop/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,7 +25,23 @@ namespace Desktop
         {
             InitializeComponent();
         }
+        public class Validate
+        {
+            private readonly string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
 
+            public bool ValidateEmail(string email)
+            {
+                if (string.IsNullOrEmpty(email))
+                    return false;
+
+                return Regex.IsMatch(email, emailPattern);
+            }
+            public bool ValidatePassword(string Password)
+            {
+                return !string.IsNullOrWhiteSpace(Password) && Password.Length >= 6;
+            }
+
+        }
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -37,9 +54,37 @@ namespace Desktop
 
         private void LogIn(object sender, RoutedEventArgs e)
         {
-           Main_empty main_empty = new Main_empty();
+            string email = TBEmail.Text;
+            string password = TBPassword.Text;
+            Validate validate = new Validate();
+            string errorMessages = "";
+
+            if (!validate.ValidatePassword(password))
+            {
+                errorMessages += "Пароль должен содержать не менее 6 символов. ";
+            }
+
+            if (!validate.ValidateEmail(email))
+            {
+                errorMessages += "Не существует такой почты. ";
+            }
+            
+            
+           /* if (!string.IsNullOrEmpty(errorMessages))
+            {
+                MessageBox.Show(errorMessages);
+            }
+            else
+            {
+                Main_empty Main_empty = new Main_empty();
+                Main_empty.Show();
+                this.Close();
+
+            } */
+            
+            Main_empty main_empty = new Main_empty();
            main_empty.Show();
-           this.Close();
+           this.Close(); 
         }
 
         private void Registation(object sender, RoutedEventArgs e)
